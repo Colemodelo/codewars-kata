762178cc9bdc1c5018eb9cf4cd36567ae7b12413
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-12 12:08:14 -0600

    Create Twice as old.js

---
+++ 8kyu/Twice as old.js
@@ -0,0 +1,7 @@
+// Your function takes two arguments:
+
+// 1. current father's age (years)
+// 2. current age of his son (years)
+
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs(dadYearsOld - (sonYearsOld * 2))
