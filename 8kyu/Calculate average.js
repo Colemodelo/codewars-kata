9508640993af10b4543d3980d724beba80a93570
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-12 12:14:13 -0600

    Create Calculate average.js

---
+++ 8kyu/Calculate average.js
@@ -0,0 +1,4 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+const find_average = array => array.reduce((a, b) => a + b) / array.length
