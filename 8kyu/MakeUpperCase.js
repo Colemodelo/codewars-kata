916253519fc7ab527617bd53a7d50e4dc1eac9c3
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-12 12:22:06 -0600

    Create MakeUpperCase.js

---
+++ 8kyu/MakeUpperCase.js
@@ -0,0 +1,6 @@
+// Write a function which converts the input string to uppercase.
+function makeUpperCase(str) {
+  // P.L.O. style, hazardous
+  // 'Cause I wreck this, dangerous
+  return str.toUpperCase()
+}
