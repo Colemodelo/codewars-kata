36845531f84eb07d768c56309a5268e98b4a1008
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-17 16:35:00 -0600

    Create Thinkful - String Drills: Repeater.js

---
+++ 7kyu/Thinkful - String Drills: Repeater.js
@@ -0,0 +1,15 @@
+// Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+
+// Example:
+// Repeater.repeat("a", 5)
+// should return
+
+// "aaaaa"
+const repeater = (string, n) => {
+  let newStr = ''
+  while ( n > 0 ) {
+    newStr += string
+    n--
+  }
+  return newStr
+}
