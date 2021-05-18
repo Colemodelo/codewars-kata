16ea45e20ae4e85e22032b650fc3ff6785d89f8f
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-18 01:10:41 -0600

    Create Hello World - Without Strings.js

---
+++ 7kyu/Hello World - Without Strings.js
@@ -0,0 +1,13 @@
+// You need to create a function, helloWorld, that will return the String Hello, World! without actually using raw strings. This includes quotes, double quotes and template strings. You can, however, use the String constructor and any related functions.
+
+// You cannot use the following:
+
+// "Hello, World!"
+// 'Hello, World!'
+// `Hello, World!`
+// Good luck and try to be as creative as possible!
+const helloWorld = () => {
+  let x = String(/Hello, World!/)
+  x = x.substring(1, x.length - 1)
+  return x
+};
