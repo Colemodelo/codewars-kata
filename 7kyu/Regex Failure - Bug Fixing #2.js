a312a6d02e9e949b24340bfe5c155c7a9bee529a
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-18 01:17:58 -0600

    Create Regex Failure - Bug Fixing #2.js

---
+++ 7kyu/Regex Failure - Bug Fixing #2.js
@@ -0,0 +1,2 @@
+// Oh no, Timmy's received some hate mail recently but he knows better. Help Timmy fix his regex filter so he can be awesome again!
+const filterWords = phrase => phrase.replace(/bad|mean|ugly|horrible|hideous/gi,'awesome')
