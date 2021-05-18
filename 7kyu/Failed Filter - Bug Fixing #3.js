704ae8f7ad90638daae22a84ebacc43bf28d0be2
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-18 01:16:43 -0600

    Create Failed Filter - Bug Fixing #3.js

---
+++ 7kyu/Failed Filter - Bug Fixing #3.js
@@ -0,0 +1,2 @@
+// Oh no, Timmy's filter doesn't seem to be working? Your task is to fix the FilterNumber function to remove all the numbers from the string.
+const FilterNumbers = str => str.split('').join('').replace(/[0-9]/g, '')
