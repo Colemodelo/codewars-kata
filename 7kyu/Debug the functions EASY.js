e1f75597ea09f69ecc40b66f3798dc7f40ca9864
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-18 01:15:19 -0600

    Create Debug the functions EASY.js

---
+++ 7kyu/Debug the functions EASY.js
@@ -0,0 +1,8 @@
+// Should be easy, begin by looking at the code. Debug the code and the functions should work.
+
+// There are three functions: Multiplication (x) Addition (+) and Reverse (!esreveR)
+const multi = arr => arr.reduce((a,b) => a * b)
+
+const add = arr => arr.reduce((a,b) => a + b)
+
+const reverse = str => str.split('').reverse().join('')
