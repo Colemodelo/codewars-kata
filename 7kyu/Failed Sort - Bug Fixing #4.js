68d6f0152aea785755607a5cf7f707c78672908b
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-18 01:13:04 -0600

    Create Failed Sort - Bug Fixing #4.js

---
+++ 7kyu/Failed Sort - Bug Fixing #4.js
@@ -0,0 +1,2 @@
+// Oh no, Timmy's Sort doesn't seem to be working? Your task is to fix the sortArray function to sort all numbers in ascending order
+let sortArray = value => value.split('').sort().join('')
