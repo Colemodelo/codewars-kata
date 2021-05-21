97543bd08010ae91370672196091c2e8e2b16824
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-21 00:41:41 -0600

    Create Training JS #7: if...else and ternary operator.js

---
+++ 8kyu/Training JS #7: if...else and ternary operator.js
@@ -0,0 +1,26 @@
+// Task:
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accept 1 parameters:n, n is the number of customers to buy hotdogs, different numbers have different prices (refer to the following table), return a number that the customer need to pay how much money.
+
+// +---------------+-------------+
+// |  numbers n    | price(cents)|
+// +---------------+-------------+
+// |n<5            |    100      |
+// +---------------+-------------+
+// |n>=5 and n<10  |     95      |
+// +---------------+-------------+
+// |n>=10          |     90      |
+// +---------------+-------------+
+// You can use if..else or ternary operator to complete it.
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+function saleHotdogs(n){
+  if(n < 5){
+    return n * 100
+  }else if(n >= 5 && n < 10){
+    return n * 95
+  }else{
+    return n * 90
+  }
+}
