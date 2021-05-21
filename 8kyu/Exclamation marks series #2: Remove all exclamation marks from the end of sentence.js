d2c1c40eb702d5d4527fbfd91e20a0f76685fa1c
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-21 00:47:31 -0600

    Create Exclamation marks series #2: Remove all exclamation marks from the end of sentence.js

---
+++ 8kyu/Exclamation marks series #2: Remove all exclamation marks from the end of sentence.js
@@ -0,0 +1,11 @@
+// Description:
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+const remove = s => s.replace(/!*$/g, "")
