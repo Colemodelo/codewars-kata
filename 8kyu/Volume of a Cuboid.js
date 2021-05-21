e50e2eb72297530db3158196cfb23e4600567002
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-21 00:09:36 -0600

    Create Volume of a Cuboid.js

---
+++ 8kyu/Volume of a Cuboid.js
@@ -0,0 +1,6 @@
+// Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+class Kata {
+   static getVolumeOfCuboid(l, w, h) {
+     return l * w * h
+   }
+}
