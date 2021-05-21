c8b678dbfc1ffe80edfcd3a53867a7c57597423b
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-20 18:05:24 -0600

    Create Find numbers which are divisible by a given number.js

---
+++ 8kyu/Find numbers which are divisible by a given number.js
@@ -0,0 +1,5 @@
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+
+// Example
+// divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
+const divisibleBy = (numbers, divisor) => numbers.filter( x => x  % divisor === 0 )
