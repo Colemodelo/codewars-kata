f4e4d5739c52b3a2d9b2012db801d9c468c2f4cb
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-20 23:27:17 -0600

    Create USD => CNY.js

---
+++ 8kyu/USD => CNY.js
@@ -0,0 +1,11 @@
+// Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+
+// For Example:
+
+//   usdcny(15) // => '101.25 Chinese Yuan'
+//   usdcny(465) // => '3138.75 Chinese Yuan'
+// The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers shold be rounded to the nearest hundredth (e.g. 21.00 NOT 21.0 or 21) (HINT: google toFixed)
+function usdcny(usd) {
+  let cny = usd / .148148148
+  return `${cny.toFixed(2)} Chinese Yuan`
+}
