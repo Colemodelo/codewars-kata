901c5a5f4e122862fa1374bbebd2c6194679050c
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-20 23:47:14 -0600

    Create Switch it Up!.js

---
+++ 8kyu/Switch it Up!.js
@@ -0,0 +1,41 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+function switchItUp(number){
+  switch(number){
+      case 9 :
+        return 'Nine'
+        break;
+      case 8 :
+        return 'Eight'
+        break;
+      case 7 :
+        return 'Seven'
+        break;
+      case 6 :
+        return 'Six'
+        break;
+      case 5 :
+        return 'Five'
+        break;
+      case 4 :
+        return 'Four'
+        break;
+      case 3 :
+        return 'Three'
+        break;
+      case 2 :
+        return 'Two'
+        break;
+      case 1 :
+        return 'One'
+        break;
+      case 0 :
+        return 'Zero'
+        break;
+  }
+}
