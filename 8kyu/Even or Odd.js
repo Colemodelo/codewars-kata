e6df9ccec2170862454a88c3cce91b2b08b45a64
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-21 00:18:31 -0600

    Create Even or Odd.js

---
+++ 8kyu/Even or Odd.js
@@ -0,0 +1,2 @@
+// Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+const even_or_odd = number => (number % 2 === 0) ? 'Even' : 'Odd'
