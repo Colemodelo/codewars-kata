821df755f51dd60c5a72ee87c853653f32db3e16
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-20 23:18:15 -0600

    Create get character from ASCII Value.js

---
+++ 8kyu/get character from ASCII Value.js
@@ -0,0 +1,7 @@
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65) # => 'A'
+// For ASCII table, you can refer to http://www.asciitable.com/
+const getChar = c => String.fromCharCode(c)
