dc36c6da61f5d9d0f3fa29d2fadbf3d89c2f5eec
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-20 18:26:30 -0600

    Create Convert a Number to a String!.js

---
+++ 8kyu/Convert a Number to a String!.js
@@ -0,0 +1,8 @@
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+const numberToString = num => String(num)
