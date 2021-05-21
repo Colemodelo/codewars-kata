1c6c4ab8e727850d4cc9d661fd0e18605e935f15
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-21 00:56:45 -0600

    Create Grasshopper - Combine strings.js

---
+++ 8kyu/Grasshopper - Combine strings.js
@@ -0,0 +1,10 @@
+// Combine strings function
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+// returns:
+
+// 'James Stevens'
+const combineNames = (n1,n2) => `${n1} ${n2}`
