35e8891848c6151ab2c9c2ea2306817bad5bd4d6
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-05-21 00:55:12 -0600

    Create Training JS #6: Basic data types--Boolean and conditional statements if..else.js

---
+++ 8kyu/Training JS #6: Basic data types--Boolean and conditional statements if..else.js
@@ -0,0 +1,13 @@
+// Task
+// Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional statement if...else, if val value is false (val==false or it can convert to false), should return a string "false", if not, return a string "true".
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+function trueOrFalse(val){
+  if(val == false || val === undefined || val === null){
+    return 'false'
+  }else{
+    return 'true'
+  } 
+}
