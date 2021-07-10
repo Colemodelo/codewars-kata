90c874b850db87efe545a4ece515dc375956be6b
Cole Modelo <70457464+Colemodelo@users.noreply.github.com>
2021-07-09 23:30:15 -0600

    Create Kata Example Twist.js

---
+++ 8kyu/Kata Example Twist.js
@@ -0,0 +1,6 @@
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites/Websites 1,000 times.
+
+// var websites = []
+var websites = Array(1000).fill('codewars')
