ed7058abeda048816dd44ba1c27a74e14af9a2af
Michael Rudloff <70457464+Colemodelo@users.noreply.github.com>
2021-11-12 16:15:40 -0700

    Create Absent Vowels.js

---
+++ 7kyu/Absent Vowels.js
@@ -0,0 +1,16 @@
+// Your job is to figure out the index of which vowel is missing from a given string:
+
+// A has an index of 0,
+// E has an index of 1,
+// I has an index of 2,
+// O has an index of 3,
+// U has an index of 4.
+// Notes: There is no need for string validation and every sentence given will contain all vowels but one. Also, you won't need to worry about capitals.
+const absentVowel = str => {
+  let vowels = 'aeiou';
+  for(let i = 0; i < vowels.length; i++){
+    if(str .indexOf(vowels[i]) == -1){
+      return i
+    }
+  }
+}
