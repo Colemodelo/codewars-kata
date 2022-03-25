478abff891d43f165541329f1d4b3516f2b1ee69
Michael Rudloff <70457464+Colemodelo@users.noreply.github.com>
2022-03-25 14:23:51 -0600

    Create ASCII Total.js

---
+++ 8kyu/ASCII Total.js
@@ -0,0 +1,13 @@
+// You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all ASCII characters.
+
+// examples:
+
+// uniTotal("a") == 97 uniTotal("aaa") == 291
+
+const uniTotal = str => {
+  let total = 0;
+  for(let i = 0; i < str.length; i++){
+    total += str[i].charCodeAt(0)
+  }
+  return total
+}
